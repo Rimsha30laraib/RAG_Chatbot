eec395fecc39807c60e4d466014dd626aa241ae8
Rimsha30laraib <rimshalarib33@gmail.com>
2026-06-02 21:27:42 +0500

    add req.txt

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,7 +1,3 @@
-# 
-
-
-
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
